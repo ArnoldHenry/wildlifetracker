aaf1f225e9d37e0664a5df38730d61e029b0d42c
ArnoldHenry <arnoldomwoyo2@gmail.com>
2018-10-23 14:46:02 +0300

    added procfile

--- src/main/java/wildlifetracker.java
+++ src/main/java/wildlifetracker.java
@@ -9,15 +9,15 @@ import static spark.debug.DebugScreen.enableDebugScreen;
 
 public class wildlifetracker {
     public static void main(String[] args) {
-//        ProcessBuilder process = new ProcessBuilder();
-//        Integer port;
-//        if (process.environment().get("PORT") != null) {
-//            port = Integer.parseInt(process.environment().get("PORT"));
-//        } else {
-//            port = 4567;
-//        }
-//
-//        setPort(port);
+        ProcessBuilder process = new ProcessBuilder();
+        Integer port;
+        if (process.environment().get("PORT") != null) {
+            port = Integer.parseInt(process.environment().get("PORT"));
+        } else {
+            port = 4567;
+        }
+
+        setPort(port);
 
         String layout = "templates/layout.vtl";
         staticFileLocation ("/public");
